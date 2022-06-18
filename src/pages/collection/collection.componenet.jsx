ed4c64b79f20b5aab275140c9fe460b51c9ae10a
Ramy19999 <ramiassi09@gmail.com>
2022-06-19 01:52:02 +0300

    add

--- src/pages/collection/collection.componenet.jsx
+++ src/pages/collection/collection.componenet.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { connect } from "react-redux/es/exports";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 import './collection.styles.scss';
-
+import CollectionItem from '../../components/collection-item/collection-item.componenet';
 const CollectionPage = ({collection}) => {
+    const {title,items} = collection;
     console.log(collection);
 return(
     <div className="collection-page">
-        <h2>Collection Page</h2>
+        <h2 className="title">{title}</h2>
+        <div>
+            {items.map(item => (
+                <CollectionItem key={item.id} item ={item}/>
+            ))}
+        </div>
     </div>
-)};
+);
+};
 
 const mapStateToProps = (state,ownProps) => ({
     collection : selectCollection(ownProps.match.params.collectionId)(state)
