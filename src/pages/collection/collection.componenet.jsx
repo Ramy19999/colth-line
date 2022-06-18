da7ffc37c842bf3a6cb69b2d44948406bbc19812
Ramy19999 <ramiassi09@gmail.com>
2022-06-19 01:32:25 +0300

    update files

--- src/pages/collection/collection.componenet.jsx
+++ src/pages/collection/collection.componenet.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { connect } from "react-redux/es/exports";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 import './collection.styles.scss';
-const CollectionPage = ({path}) => {
-    console.log(path)
+
+const CollectionPage = ({collection}) => {
+    console.log(collection);
 return(
     <div className="collection-page">
         <h2>Collection Page</h2>
     </div>
 )};
 
-//const mapStateToProps = (state,ownProps) => ({
-  //  collection : selectCollection(ownProps.match.params.collectionId)(state)
-//})
+const mapStateToProps = (state,ownProps) => ({
+    collection : selectCollection(ownProps.match.params.collectionId)(state)
+})
 
-export default CollectionPage;
+export default connect(mapStateToProps)(CollectionPage);
